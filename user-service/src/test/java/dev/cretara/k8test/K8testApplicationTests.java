24d259c2dcf9b26935bb313f94ba0dcb69f0f612
cretara <pierdaniele.cretara@lviiier.com>
2022-07-07 14:07:49 +0200

    Initial commit

---
+++ user-service/src/test/java/dev/cretara/k8test/K8testApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cretara.k8test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8testApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
